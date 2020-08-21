08ecd631c893ae7b4b76fc6d85d16b1c1d145c0f
Win <jvictor.braga@yahoo.com>
2020-08-21 14:29:47 -0300

    criado instancias de nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,15 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=28):
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
         return f'olá {id(self)}'
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('raiane')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'joão'
+    print(p.nome)
+    print(p.idade)
